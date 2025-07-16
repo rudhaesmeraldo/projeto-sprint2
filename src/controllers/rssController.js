5e65e04ac713f81c314b3136d5145f796a20f45c
Yuri Kiev de Sousa Barreto <yurikievbarreto@gmail.com>
2025-07-16 16:58:28 -0300

    feat: traduz e salva  o conteudo rss extraido em um arquivo .json no diretorio 'data'

--- src/controllers/rssController.js
+++ src/controllers/rssController.js
@@ -1,19 +1,28 @@
 const Parser = require('rss-parser');
 const parser = new Parser();
+const {traduzConteudo} = require('../services/deeplService')
+const fs = require('fs');
+const { json } = require('stream/consumers');
 
 async function obterNoticias(req, res) {
   try {
     const feed = await parser.parseURL('https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss');
 
-    const noticias = feed.items.map(item => ({
-      titulo: item.title,
+    let noticiasPromises = feed.items
+    .slice(0,8)
+    .map(async item => ({
+      titulo: await traduzConteudo(item.title),
       link: item.link,
       data: item.pubDate,
-      descricao: item.contentSnippet,
-      imagem: item.enclosure?.url || null
+      descricao: await traduzConteudo(item.contentSnippet),
+      imagem:item.enclosure?.url || null
     }));
 
-    res.json(noticias);
+
+    
+    let noticiasTraduzidas = await Promise.all(noticiasPromises)
+    fs.writeFileSync('./src/data/noticias-pt-br.json', JSON.stringify(noticiasTraduzidas)) 
+    res.json(noticiasTraduzidas);
   } catch (erro) {
     console.error('Erro ao buscar o feed:', erro.message);
     res.status(500).json({ erro: 'Erro ao obter feed RSS' });
---
+++ src/services/deeplService.js
@@ -0,0 +1,19 @@
+const deepl = require('deepl-node')
+
+// IMPORTANTE: lembrar de passar a chave para o arquivo .env quando for finalizar o projeto 
+const chaveAPI = "2dfcc874-068a-4bdd-a568-14f2c1fb3c12:fx";
+// IMPORTANTE: lembrar de passar a chave para o arquivo .env quando for finalizar o projeto 
+
+const tradutor = new deepl.Translator(chaveAPI);
+
+
+async function traduzConteudo(texto){
+  try{
+    const resultado = await tradutor.translateText(texto, null, 'pt-br');
+    return resultado.text
+  }catch(error){
+    console.log(`Erro ao tentar traduzir conteúdo: ${error}`)
+  }
+}
+
+module.exports = {traduzConteudo}
