ca9245e13a4f276e055fe63a89a13c5918cfa543
rudha <rudhaesmeraldo@gmail.com>
2025-07-16 14:45:02 -0300

    🔧 chore: Integração da rota /rss no arquivo app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const rssRoute = require('./src/routes/rssRoute');
+
+app.use(express.json());
+app.use('/rss', rssRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`👌 Servidor rodando na porta ${PORT} | http://localhost:3000/rss`);
+});
