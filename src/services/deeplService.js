0e7d8d48ebc9db1ca4e3d71674fda906a2a8993c
rudha <rudhaesmeraldo@gmail.com>
2025-07-18 15:35:06 -0300

    🔀 merge: integra alterações no rssController.js, rssRoute.js, deeplService.js da branch Rafaela

--- src/services/deeplService.js
+++ src/services/deeplService.js
@@ -1,16 +1,18 @@
 const deepl = require('deepl-node');
 require('dotenv').config();
 
-const chaveAPI = process.env.chaveAPI;
+// IMPORTANTE: lembrar de passar a chave para o arquivo .env quando for finalizar o projeto 
+const chaveAPI = "2dfcc874-068a-4bdd-a568-14f2c1fb3c12:fx";
+// IMPORTANTE: lembrar de passar a chave para o arquivo .env quando for finalizar o projeto 
+
 const tradutor = new deepl.Translator(chaveAPI);
 
 async function traduzConteudo(texto) {
   try {
     const resultado = await tradutor.translateText(texto, null, 'pt-br');
-    return resultado.text;
-  } catch (error) {
-    console.error(' Erro ao traduzir conteúdo:', error.message);
-    return texto; //retorna original se erro
+    return resultado.text
+  }catch(error){
+    console.log(`Erro ao tentar traduzir conteúdo: ${error}`)
   }
 }
 
