80c9c85413f7881d00143fc8dcc2f9dee0a1745a
rudha <rudhaesmeraldo@gmail.com>
2025-07-21 14:16:26 -0300

    🔐 refactor: mover chave da API do DeepL para .env por segurança

--- src/services/deeplService.js
+++ src/services/deeplService.js
@@ -1,18 +1,16 @@
 const deepl = require('deepl-node');
 require('dotenv').config();
 
-// IMPORTANTE: lembrar de passar a chave para o arquivo .env quando for finalizar o projeto 
-const chaveAPI = "2dfcc874-068a-4bdd-a568-14f2c1fb3c12:fx";
-// IMPORTANTE: lembrar de passar a chave para o arquivo .env quando for finalizar o projeto 
-
+const chaveAPI = process.env.chaveAPI;
 const tradutor = new deepl.Translator(chaveAPI);
 
 async function traduzConteudo(texto) {
   try {
     const resultado = await tradutor.translateText(texto, null, 'pt-br');
-    return resultado.text
-  }catch(error){
-    console.log(`Erro ao tentar traduzir conteúdo: ${error}`)
+    return resultado.text;
+  } catch (error) {
+    console.error(' Erro ao traduzir conteúdo:', error.message);
+    return texto; //retorna original se erro
   }
 }
 
